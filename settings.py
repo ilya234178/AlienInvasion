aae78b378325b74ccfe6f7122c9a00681b3afad3
Ilya2311 <veruhanov00@mail.ru>
2025-10-04 09:59:57 +0300

    save settings

--- settings.py
+++ settings.py
@@ -4,7 +4,14 @@ class Settings:
         """Инициализирует настройки игры."""
         # Параметры экрана
         self.screen_width = 1200
-        self.screen_heigth = 800
+        self.screen_height = 800
         self.bg_color = (0, 0, 255)
 
         self.ship_speed = 5
+
+        #параметры снаряда
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 5
